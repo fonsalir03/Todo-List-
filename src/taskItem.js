96bea613d77ba0d602cef8dc432d90e6d44d651b
fonsalir03 <weht03anon@gmail.com>
2025-03-26 13:58:48 -0500

    convert TaskItem to a class

--- src/taskItem.js
+++ src/taskItem.js
@@ -1,42 +1,60 @@
 import {isPast} from "date-fns";
 
-function TaskItem(title, description, dueDate, priority){
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.completed = false;
+class TaskItem{
+    _title = undefined
+    _description = undefined
+    _dueDate = undefined
+    _priority = undefined
+    completed = false;
 
-    this.updateTitle = (function(newTitle){
-        this.title = newTitle;
-    });
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate
+        this.priority = priority
+    }
+
+    get title(){
+        return this._title
+    }
+    set title(newTitle){
+        this._title = newTitle;
+    }
     
-    this.updateDescription = (function(newDescription){
-        this.description = newDescription;
-    });
+    get description(){
+        return this._description;
+    }
+    set description(newDescription){
+        this._description = newDescription
+    }
 
-    this.updateDueDate = (function(year, month, day){
-        const monthIndex = month - 1;
+    get dueDate(){
+        return this._dueDate
+    }
+    set dueDate(dateArr){
+        const year = dateArr[0], monthIndex = dateArr[1] - 1, day = dateArr[2];
         const newDate = new Date(year, monthIndex, day);
         if (isPast(newDate) == false){
-            this.dueDate = newDate;
+            this._dueDate = newDate
         }else{
             console.log("Due date must be a day that has not passed!");
         }
-    });
+    }
 
-    this.updatePriority = (function(newPriority){
+    get priority(){
+        return this._priority;
+    }
+    set priority(newPriority){
         const MAX_PRIORITY = 3
         if (typeof newPriority != "number"){
             console.log("invalid input, must be number");
             return
         }else if (newPriority >= 1 && newPriority <= MAX_PRIORITY){
-            this.priority = newPriority;
+            this._priority = newPriority;
         }
-        
-    });
+    }
 
-    this.toggleCompleted = (function(){
+    toggleCompleted = (function(){
         this.completed = !this.completed;
     });
 
