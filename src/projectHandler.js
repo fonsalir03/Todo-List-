a6e08c1cf29dccc200e4e998b6128775ad44dcea
fonsalir03 <weht03anon@gmail.com>
2025-04-06 20:44:12 -0500

    make all properties truly private remove "_selectedProject" add default project to the projects property

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -2,41 +2,41 @@ import { TaskItem } from "./taskItem";
 import { Project } from "./project";
 
 class ProjectHandler{
-    _projectIndex = 0;
-    _projects = [];
+    #projectIndex = 0;
+    #projects = [];
 
     constructor(){
-        this._selectedProject = new Project("default");
+        this.#projects.push(new Project("default"))
     }
 
     get currentProject(){
-        return this._projects[this._projectIndex];
+        return this.#projects[this.#projectIndex];
     }
 
     add(ProjectName){
-        this._projects.push(new Project(ProjectName));
+        this.#projects.push(new Project(ProjectName));
     }
 
     next(){
-        if (this._projects.length - 1 > this._projectIndex){
-            this._projectIndex += 1;
+        if (this.#projects.length - 1 > this.#projectIndex){
+            this.#projectIndex += 1;
         } 
     }
 
     prev(){
-        if (this._projectIndex > 0){
-            this._projectIndex -=1;
+        if (this.#projectIndex > 0){
+            this.#projectIndex -=1;
         }
     }
 
     delete(){
         //references to project length and the current index
-        const ProjLen = this._projects.length;
-        const ProjIndex = this._projectIndex;
+        const ProjLen = this.#projects.length;
+        const ProjIndex = this.#projectIndex;
         if ( ProjLen == 0){
             return;
         }
-        this._projects.splice(ProjIndex,1)
+        this.#projects.splice(ProjIndex,1)
         //if the currently selected project is at the end of the array, move the previous
         if(ProjIndex == ProjLen - 1){
             this.prev();
