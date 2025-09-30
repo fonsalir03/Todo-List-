d719c0822ec906c0bcd826039529968809b11023
fonsalir03 <mdav121503@gmail.com>
2025-09-30 11:50:29 -0500

    create component that will request input from user for task or project creation

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,14 +1,13 @@
 
 
-export class domHandler{
+export class DomHandler{
 
     constructor(){
+        // content that is constant to the page
         this.CreateMainContainers();
         this.CreateToolbarButtons();
-        this.CreateTaskForm();
         //testing
-        this.taskDialog.show()
-        
+
         document.body.appendChild(this.mainContentDiv)
     }
     
@@ -67,31 +66,80 @@ export class domHandler{
         this.projectSectionDiv.appendChild(this.viewProjectsButton);
     }
 
-    CreateTaskForm(){
-        //create task item dialog so the user can close the form when submitted
-        this.taskDialog = document.createElement("dialog");
-        this.taskDialog.setAttribute("id", "task-dialog");
+    CreateForm(type){
+        //check for if the form is for a task item or project
 
-        //create the task form, nested in taskDialog
-        this.taskForm = document.createElement("form");
-        this.taskForm.setAttribute("id", "task-form");
-        this.taskDialog.appendChild(this.taskForm);
-        //--just start with creating the elements, worry about the attributes later
-        //nested in the form
-        //  create the title input using textarea
+        // dialog
+        this.dialog = document.createElement("dialog");
+        this.dialog.setAttribute("id", "data-dialog");
 
-        //  create the description input
+        // form
+        this.form = document.createElement("form");
+        this.form.setAttribute("id", "data-form");
+        this.dialog.appendChild(this.form);
 
-        //  create the due date date picker
+        //  form title
+        this.formTitleInput = document.createElement("input");
+        this.formTitleInput.setAttribute("type", "text");
+        this.formTitleInput.setAttribute("id", "form-title");
+        this.form.appendChild(this.formTitleInput);
 
-        //  create label for the priority "How important is this task?" "What is the priority?"
-        //  create the priority selection
+        //  form description
+        this.formDescriptionInput = document.createElement("textarea");
+        this.formDescriptionInput.setAttribute("id", "form-description");
+        this.form.appendChild(this.formDescriptionInput);
+        
+        if (type == "task"){
+            //  form date picker
+            this.formDateInput = document.createElement("input");
+            this.formDateInput.setAttribute("type", "date");
+            this.formDateInput.setAttribute("id", "form-date");
+            this.form.appendChild(this.formDateInput);
 
-        //  create the cancel button
+            //  priority
+            this.formPriorityLabel = document.createElement("label");
+            this.formPriorityLabel.textContent = "How important is this task?";
+            this.formPriorityLabel.setAttribute("for","form-priority");
+            this.form.appendChild(this.formPriorityLabel);
 
-        //  create the submit button
+            //  create the priority selection
+            this.formPriority = document.createElement("select");
+            this.formPriority.setAttribute("name", "form-priortity");
 
-        document.body.appendChild(this.taskDialog)
+            // priority option 1 : low
+            this.formPriorityOption1 = document.createElement("option");
+            this.formPriorityOption1.value = 1;
+            this.formPriorityOption1.textContent = "low";
+            this.formPriority.appendChild(this.formPriorityOption1);
+
+            // priority option 2 : medium
+            this.formPriorityOption2 = document.createElement("option");
+            this.formPriorityOption2.value = 2;
+            this.formPriorityOption2.textContent = "medium";
+            this.formPriority.appendChild(this.formPriorityOption2);
+
+            // priority option 3 : high
+            this.formPriorityOption3 = document.createElement("option");
+            this.formPriorityOption3.value = 3;
+            this.formPriorityOption3.textContent = "high";
+            this.formPriority.appendChild(this.formPriorityOption3);
+
+            this.form.appendChild(this.formPriority);
+        }
+        
+        //Submit button
+        this.formSubmitButton = document.createElement("button");
+        this.formSubmitButton.setAttribute("id", "form-submit-button");
+        this.formSubmitButton.textContent = "Submit";
+        this.form.appendChild(this.formSubmitButton);       
+
+        //  create the Cancel button
+        this.formCancelButton = document.createElement("button");
+        this.formCancelButton.setAttribute("id", "form-cancel-button");
+        this.formCancelButton.textContent = "Cancel";
+        this.form.appendChild(this.formCancelButton);
+
+        document.body.appendChild(this.dialog); 
     }
 
 }
