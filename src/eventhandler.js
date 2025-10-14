e35aaaafd4e0436b9ef8dc46c64f86d23d2bf5f1
fonsalir03 <mdav121503@gmail.com>
2025-10-14 13:08:22 -0500

    add functionality to add project button

--- src/eventhandler.js
+++ src/eventhandler.js
@@ -1,1 +1,20 @@
-//pass in the domhandler and projecthandler instance and use its properties to reference needed elements
+//pass in the domhandler and projecthandler instance
+export class EventHandler{
+    
+    constructor(dom, projectHandler){
+        this.projectHandler = projectHandler;
+        this.addListeners(dom)
+    }
+
+    addListeners(dom){
+
+        //shows form when "Create Project" button is clicked
+        dom.createProjectButton.addEventListener("click", function(){
+            // checks if the form's parent already exists using live reference
+            if (document.body.contains(dom.dialog) == false){
+                dom.CreateForm("project");
+            }
+        });
+
+    }
+};
