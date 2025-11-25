e7210a4eaa51dfe8f6a16b9283889540f107d163
fonsalir03 <mdav121503@gmail.com>
2025-11-25 13:14:18 -0600

    add method to find index by title add method to handle project form input add method to handle task form input modified dom property to be accessed through the class instead of being passed as an argument add current project property

--- src/eventhandler.js
+++ src/eventhandler.js
@@ -1,42 +1,100 @@
-//pass in the domhandler and projecthandler instance
 export class EventHandler{
     
     constructor(dom, projectHandler){
+        this.dom = dom
         this.projectHandler = projectHandler;
-        this.addListeners(dom)
+        this.addListeners(this.dom)
+        this.currentProject = undefined
     }
 
-    addListeners(dom){
-
-        //shows form when "Create Project" button is clicked
-        dom.createProjectButton.addEventListener("click", function(){
-            // checks if the form's parent already exists using live reference
-            if (document.body.contains(dom.dialog) == false){
-                dom.CreateForm("project");
-            }
-        });
-
-        document.body.addEventListener("click", function(event){
-            if (event.target.id == "form-cancel-button"){
-                dom.dialog.remove()
-            }
-        });
-        
-        document.body.addEventListener("submit", (event) => {
-            event.preventDefault()
-            const formData = new FormData(dom.form)
-
-            //read the "data-type" on the form and use that to determine what handler to call
-            const projectTitle = formData.get("title");
-            const projectDescription = formData.get("description");
-            dom.renderProject(this.projectHandler.add(projectTitle, projectDescription));
-
-            dom.dialog.remove()
-
-        })
-
+    findIndex(title, holder){
+        return holder.items.findIndex(item => item.name === title)
     }
 
     // callback functions
-    // move the event listener call back functions to their own functions for easy modification
+
+    projectFormHandler(formData){
+        
+        const projectTitle = formData.get("title");
+        const projectDescription = formData.get("description");
+        const newProject = this.projectHandler.create(projectTitle, projectDescription);
+
+        this.dom.renderProject(newProject);
+
+    }
+
+    taskFormHandler(event, formData){
+
+        //form inputs
+        const taskTitle = formData.get("title");
+        const taskDescription = formData.get("description");
+        const taskDueDate = formData.get("date");
+        const taskPriority = formData.get("priority");
+
+        const projectIndex = event.submitter.dataset.projectIndex;
+        const newTask = this.projectHandler.items[projectIndex].add(taskTitle, taskDescription, taskDueDate, taskPriority);
+
+        // render the task that was returned from project.add()
+        this.dom.renderTask(newTask, this.currentProject);
+
+    }
+
+    formHandler(event){
+
+        event.preventDefault();
+        const formType = event.target.dataset.type;
+        const formData = new FormData(this.dom.form);
+
+        if (formType == "task"){
+            this.taskFormHandler(event,formData);
+        }
+        if (formType == "project"){
+            this.projectFormHandler(formData);
+        }
+        
+        this.dom.dialog.remove();
+
+    }
+
+    addListeners(){
+
+        document.body.addEventListener("click", (event) => {
+            // checks if a dialog is already rendered on the DOM
+            if (document.body.contains(this.dom.dialog) == false){
+                // create project button click event
+                if (event.target.id == "create-project"){
+                    this.dom.CreateForm("project");
+                }
+                // create task button click event
+                if (event.target.id == "project-add-task-button"){
+
+                    const DOMProject = event.target.parentElement;
+                    const DOMProjectTitle = DOMProject.children[0].textContent;
+                    const projectIndex = this.findIndex(DOMProjectTitle, this.projectHandler);
+                    
+                    //changing the selected project for the instance and dom variant
+                    this.projectHandler.select(projectIndex);
+                    this.currentProject = DOMProject;
+
+                    this.dom.CreateForm("task", projectIndex);
+
+                }
+            }
+
+        });
+
+        //checks if form is being closed
+        document.body.addEventListener("click", (event) => {
+            if (event.target.id == "form-cancel-button"){
+                this.dom.dialog.remove();
+            }
+        });
+        
+        //checks if form is being submitted
+        document.body.addEventListener("submit", (event)=> {
+            if (event.target.id == "data-form"){
+                this.formHandler(event);
+            }
+        })
+    }
 };
